087d8fb6b81f5dd258f15b77428d34cf4a32f80a
nagaoshuhei0811 <nagaoshuhei0811@gmail.com>
2024-07-01 16:56:16 +0900

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import yfinance as yf
+
 
 st.write("""
 # My first app
 Hello *world!*
 """)
-df = yf.download("AAPL", start="2010-11-01", end="2020-11-01")
-st.line_chart(df)
